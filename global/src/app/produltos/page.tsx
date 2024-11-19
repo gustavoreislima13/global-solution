14d2b2e5ab7fd9bf7791fd2196d338d79eac00ac
gustavoreislima13 <reisanselmo7@gmail.com>
2024-11-19 20:51:08 -0300

    tabela

--- global/src/app/produltos/page.tsx
+++ global/src/app/produltos/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+
+import { useState } from "react"
 
 export default function Produltos() {
+
+    const [produlto, setProdultos] = useState<TipoProdulto>({
+        eletrodomestico:"",
+        marca:"",
+        eficiencia_energetica:"",
+        consumo_energetico:"",
+        cpf_cliente:0.0,
+    })
+
+    const handleChange = (evento:React.ChangeEvent <HTMLInputElement>)=>{
+        const{name,value} = evento.target;
+        setProdultos({...Produltos,[name]:value})
+    }
+
 
   return (
     <div>
@@ -9,23 +26,24 @@ export default function Produltos() {
             <form>
                 <div>
                     <label htmlFor="idNn">Eletrodomestico</label>
-                    <input type="text" name="eletrodomestico" id="idNn" value="" placeholder="digite o eletrodomestico" required/>
+                    <input type="text" name="eletrodomestico" id="idNn" value={produlto.eletrodomestico} 
+                    onChange={(evento)=>  handleChange(evento)} placeholder="digite o eletrodomestico" required/>
                 </div>
                 <div>
                     <label htmlFor="idMa">Marca</label>
-                    <input type="text" name="marca" id="idMa" value="" placeholder="digite a marca" required/>
+                    <input type="text" name="marca" id="idMa" value={produlto.marca} onChange={(evento)=>  handleChange(evento)} placeholder="digite a marca" required/>
                 </div>
                 <div>
                     <label htmlFor="idEf">Eficiencia energetica</label>
-                    <input type="text" name="eficiencia_energetica" id="idEf" value="" placeholder="digite a eficiencia Energetica" required/>
+                    <input type="text" name="eficiencia_energetica" id="idEf" value={produlto.eficiencia_energetica} onChange={(evento)=>  handleChange(evento)} placeholder="digite a eficiencia Energetica" required/>
                 </div>
                 <div>
                     <label htmlFor="idCs">Consumo energetico</label>
-                    <input type="number" name="consumo_energetica" id="idCs" value="" placeholder="digite a consumo Energetica" required/>
+                    <input type="number" name="consumo_energetica" id="idCs" value={produlto.consumo_energetica} onChange={(evento)=>  handleChange(evento)} placeholder="digite a consumo Energetica" required/>
                 </div>
                 <div>
                     <label htmlFor="idCpf">CPF</label>
-                    <input type="number" name="cpf_cliente" id="idCpf" value="" placeholder="digite o cpf" required/>
+                    <input type="number" name="cpf_cliente" id="idCpf" value={produlto.cpf_cliente} onChange={(evento)=>  handleChange(evento)} placeholder="digite o cpf" required/>
                 </div>
             </form>
         </div>
---
+++ global/src/types/types.ts
@@ -0,0 +1,7 @@
+export type TipoProdulto = {
+    eletrodomestico:string;
+    marca:string;
+    eficiencia_energetica:string;
+    consumo_energetico:number
+    cpf_cliente:number
+}
