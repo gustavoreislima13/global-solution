2f9c655cef62eaee738183006d8b9f2474785fa8
gustavoreislima13 <reisanselmo7@gmail.com>
2024-11-19 23:24:48 -0300

    ultimnso ajuste tabela

--- global/src/app/produltos/page.tsx
+++ global/src/app/produltos/page.tsx
@@ -1,12 +1,27 @@
-"use client"
+"use client";
 import { TipoProdulto } from "@/types/types";
-import { headers } from "next/headers";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Produtos() {
 
     const navigate = useRouter()
+
+    const [produtos, setProdutos] = useState<TipoProdulto[]>([])
+    const chamadaApi = async () => {
+        try {
+            const response = await fetch(""); // Adicione a URL correta da API aqui.
+            const data = await response.json();
+            setProdutos(data);
+        } catch (error) {
+            console.error("Falha na listagem");
+        }
+    }
+
+    useEffect(() => {
+        chamadaApi();
+    }, [])
 
     const [produto, setProduto] = useState<TipoProdulto>({
         eletrodomestico: "",
@@ -80,10 +95,45 @@ export default function Produtos() {
                         <input type="number" name="cpf_cliente" id="idCpf" value={produto.cpf_cliente} onChange={(evento) => handleChange(evento)} placeholder="Digite o CPF" required />
                     </div>
                     <div>
-                        <button type="submit" 
-                        className="block ms-auto me-6 bg-[#4CF214] hover:bg-[#4CF214] text-white font-bold py-2 px-4 rounded">CADASTRAR</button>
+                        <button type="submit"
+                            className="block ms-auto me-6 bg-[#4CF214] hover:bg-[#4CF214] text-white font-bold py-2 px-4 rounded">Cadastra</button>
                     </div>
                 </form>
+            </div>
+            <h2>Listagem de Produtos</h2>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Eletrodoméstico</th>
+                            <th>Marca</th>
+                            <th>Eficiência energética</th>
+                            <th>Consumo energético</th>
+                            <th>CPF</th>
+                            <th>Editar | Excluir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            // Aguardando a chamada da API antes de renderizar os produtos
+                            produtos.length > 0 && produtos.map(p => (
+                                <tr key={p.cpf_cliente}>
+                                    <td>{p.eletrodomestico}</td>
+                                    <td>{p.marca}</td>
+                                    <td>{p.eficiencia_energetica}</td>
+                                    <td>{p.consumo_energetico}</td>
+                                    <td>{p.cpf_cliente}</td>
+                                    <td><Link href="/">Editar</Link>| <Link href="/">Excluir</Link></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={5}>Total de informacoes: -{produto.cpf_cliente}</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
     )
